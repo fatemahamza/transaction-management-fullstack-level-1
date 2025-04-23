8f2dc66d3ea0ef800ec555928998875496d86da6
fhamza <fatimaaqeel544@gmail.com>
2025-04-23 18:20:48 +0300

    done

--- app-express/src/server/app.js
+++ app-express/src/server/app.js
@@ -5,12 +5,7 @@ const api_endpoints = require('../api');
 const app = express();
 
 let transactions = [];
-let accounts = [
-  {
-    account_id: 1,
-    balance: 0
-  }
-];
+let accounts = {};
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,23 +13,29 @@ app.use(bodyParser.json());
 app.post("/transactions", (req, res) => {
   const {amount, account_id} = req.body;
 
-  const acount = accounts.find(acc => acc.account_id === account_id);
-  if (!acount) {
-    return res.status(404).send("Account not found");
-  }
+  const transaction_id = generateRandomId();
 
-  const transactionId = generateRandomId();
-  const transaction = {transaction_id: transactionId, account_id, amount};
+  const transaction = {
+    transaction_id,
+    account_id,
+    amount
+  };
 
   transactions.push(transaction);
 
-  acount.balance += amount;
+  if (!accounts[account_id]) {
+    accounts[account_id] = {
+      account_id,
+      balance: 0
+    };
+  }
 
-  res.status(201).json(transaction);
+  accounts[account_id].balance += amount;
 
+  res.status(201).json({transaction_id});
 });
 
-app.get("/transaction/:id", (req, res) => {
+app.get("/transactions/:id", (req, res) => {
   const {id} = req.params;
   const transaction = transactions.find(t => t.transaction_id === id);
 
@@ -47,17 +48,20 @@ app.get("/transaction/:id", (req, res) => {
 
 app.get("/accounts/:id", (req, res) => {
   const {id} = req.params;
-  const account = accounts.find(acc => acc.account_id === parseInt(id));
-
-  if (!account) {
-    return res.status(404).send("Account not found");
+  
+  if (!accounts[id]) {
+    accounts[id] = {
+      account_id: id,
+      balance: 0
+    };
   }
 
-  res.status(200).json(account);
+  res.status(200).json(accounts[id]);
 });
 
 function generateRandomId() {
-  return Math.floor(Math.random()*1000000);
+  return Math.random().toString(36).substring(2, 15) + 
+         Math.random().toString(36).substring(2, 15);
 }
 
 app.use('/', does_method_exist, api_endpoints);
